284f8810bbcba235a178dcc701fc96b7f0fd9e33
powersimmani <powersimmani@gmail.com>
2016-08-16 19:02:27 +0900

    auto commit 2016-08-16-19:02:27

--- 2.feature_generator.py
+++ 2.feature_generator.py
@@ -21,7 +21,7 @@ from pymongo import MongoClient
 #그 연도별 순위를 author에 저장한다. 
 #근데 이건 h-index랑 거의 비슷 
 
-def collection_rank(ip,port,db,collection):
+def collection_cited_count_maker(ip,port,db,collection):
 	#collection의 연도별 인용수를 저장해주는 프로그램 
 	collection_client = MongoClient(ip, port)[db][collection]
 	item_client = MongoClient(ip, port)[db]["paper"]
@@ -56,19 +56,50 @@ def collection_rank(ip,port,db,collection):
 
 		collection_client.update({"_id":collection_id["_id"]},{"$set":{"cited_count":collection_cited_count[collection_id["_id"]],"last_modified":time.time()}})
 
-	#db에서 이름 
-
-
-"""	
-def author_rank_H_index(ip,port,db):
-	author_client = MongoClient(ip, port)[db]["author"]
+def collection_rank(ip,port,db,collection):
+	collection_client = MongoClient(ip, port)[db][collection]
 	item_client = MongoClient(ip, port)[db]["paper"]
 	
 	past_time = time.time()
 	iterator = 0
-	cited_counts_per_years = []
-	for author_id in author_client.find():
-		published = author_id["published"]		
+	collection_cited_count = [[]]*collection_client.count()
+	collection_rank_percentile = {}
+	id_iter_map = {}
+	iter_id_map = {}
+	#rank는 percentile로 구해야 의미가 있을 것 같은데 아닌가?
+
+	for collection_id in collection_client.find():
+		collection_cited_count[iterator] = collection_id["cited_count"]		
+
+		#collection rank에 이름 채워넣을라고 집어넣은거 
+		collection_rank[collection_id["_id"]] = [0]*66
+		collection_rank_percentile[collection_id["_id"]] = [0]*66
+
+		id_iter_map[collection_id["_id"]] = iterator
+		iter_id_map[iterator] = collection_id["_id"]
+		#print collection_cited_count[iterator]
+		iterator+=1
+
+
+	#일반 rank와 percentile rank를 같이 구한다. 
+	for year in range(0,66):
+		cited_count_for_rank = [-item[year] for item in collection_cited_count]
+		cited_count_for_percentile = [item[year] for item in collection_cited_count]
+
+		cited_rank = rankdata(cited_count_for_rank)
+		
+		#순위와 백분위를 구한다. 
+		for item_id in id_iter_map:
+			index = id_iter_map[item_id]
+			collection_rank[item_id][year] = cited_rank[index]
+			collection_rank_percentile[item_id][year] = percentileofscore(cited_count_for_percentile, index)
+
+	print collection_rank
+	input()
+	print collection_rank_percentile
+	input()
+
+		"""
 		collection_cited = {}
 
 		if (iterator%10000 == 0 ):
@@ -76,7 +107,7 @@ def author_rank_H_index(ip,port,db):
 			past_time = time.time()
 		iterator += 1
 
-	#저자가 발행한 논문들의 연도별 인용수 리스트가 필요하다 
+		#저자가 발행한 논문들의 연도별 인용수 리스트가 필요하다 
 		for year in published:
 			item_list= published[year]
 		
@@ -88,8 +119,7 @@ def author_rank_H_index(ip,port,db):
 					if (str(year) not in collection_cited):
 						collection_cited[str(year)] = 0
 					collection_cited[str(year)] += int(cited_count[str(year)])
-
-"""
+		"""
 
 #최근 업데이트된 정보를 볼 수 있도록 시간을 추가하였다. 
 #이거 다 되면 author한번 더 돌릴 수 있도록 한다. 
@@ -98,12 +128,11 @@ ip = "127.0.0.1"
 port= 27017
 db = "DBLP_Citation_network_V8"
 #각 collection들의 연도별 cited count를 만들어 저장 -> ranking용 
+#collection_cited_count_maker(ip,port,db,"author")
+#collection_cited_count_maker(ip,port,db,"venue")
+
 #collection_rank(ip,port,db,"author")
 collection_rank(ip,port,db,"venue")
-
-
-#author_rank_H_index(ip,port,db)
-
 
 
 #test(ip,port,db)
@@ -116,3 +145,4 @@ def test(ip,port,db):
 	collection_client = MongoClient('lamda.ml', 27017)[db]["test"]
 	#collection_client.save({"_id":"1", "time":time.time(),"temp":"2312312"})
 	collection_client.update({"_id":"1"},{"$set":{"cited_count":"111112","last_modified":time.time()}})	
+	collection_rank = {}
