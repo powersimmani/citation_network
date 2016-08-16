2b139cb512e455db6ffbc7e2d17fddc5010a8183
powersimmani <powersimmani@gmail.com>
2016-08-16 16:41:51 +0900

    autoupdate 2016-08-16-16:41:51

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 count=0
-while [ $count -lt 1000000 ]
-do
+date_info = date
+echo date_info
+"""
+#while [ $count -lt 1000000 ]
+#do
     echo $count
      count=$(($count+1))
-    git add 
+    git add *
     git commit -m "auto_commit_2016_08_16 16:33"
-    sleep 500
-done
+    sleep 2000
+#done
+"""
