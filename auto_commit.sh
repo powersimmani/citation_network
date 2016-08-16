fa1264e467fe4797b307ea4cf3ea7de181485880
powersimmani <powersimmani@gmail.com>
2016-08-16 16:48:11 +0900

    auto commit 2016-08-16-16:48:11

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 count=0
-git commit -a -m "auto commit `date +%F-%T`"
-echo "please push the project"
 
-#while [ $count -lt 1000000 ]
-#do
-#    echo $count
-#     count=$(($count+1))
-#    git add *
-#    git commit -m "auto_commit_2016_08_16 16:33"
-#    sleep 2000
-#done
+
+while [ $count -lt 1000000 ]
+do
+    echo $count
+    count=$(($count+1))
+	git commit -a -m "auto commit `date +%F-%T`"
+	echo "please push the project"
+    sleep 4000
+done
