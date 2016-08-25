dad2dae8c576e5f50f9a0e3f122ba0f991f0f6ac
powersimmani <powersimmani@gmail.com>
2016-08-25 10:33:45 +0900

    auto commit 2016-08-25-10:33:45

--- 2.feature_generator.py
+++ 2.feature_generator.py
@@ -66,27 +66,24 @@ def collection_cited_count_maker(ip,port,db,collection):
 
 def collection_rank_maker(ip,port,db,collection):
 	collection_client = MongoClient(ip, port)[db][collection]
-	item_client = MongoClient(ip, port)[db]["paper"]
-	
+	#각 collection의 인용수를 기준으로 순위와 백분위를 정한다. 
 	past_time = time.time()
 	iterator = 0
 	collection_cited_count = [[]]*collection_client.count()
 	collection_rank = {}
-	collection_rank_percentile = {}
 	id_iter_map = {}
 	iter_id_map = {}
 	#rank는 percentile로 구해야 의미가 있을 것 같은데 아닌가?
 
 	for collection_id in collection_client.find():
 		if (iterator%10000 == 0):
-			print collection + " cited_count uploading : " + str(iterator) + "/" + str(collection_client.count()) +" takes: " + str((time.time() - past_time)) + " seconds"
+			print collection + " cited_count downloading : " + str(iterator) + "/" + str(collection_client.count()) +" takes: " + str((time.time() - past_time)) + " seconds"
 			past_time = time.time()
 
 		collection_cited_count[iterator] = collection_id["cited_count"]		
 
 		#collection rank에 이름 채워넣을라고 집어넣은거 
 		collection_rank[collection_id["_id"]] = [0]*66
-		collection_rank_percentile[collection_id["_id"]] = [0]*66
 
 		id_iter_map[collection_id["_id"]] = iterator
 		iter_id_map[iterator] = collection_id["_id"]
@@ -95,7 +92,7 @@ def collection_rank_maker(ip,port,db,collection):
 
 
 
-	#일반 rank와 percentile rank를 같이 구한다. 
+	#일반 rank를 구한다. 
 	for year in range(0,66):
 		print collection + " calculate ranking : "+ str(year) + "/" + str(66) +" takes: " + str((time.time() - past_time)) + " seconds"
 
@@ -106,38 +103,78 @@ def collection_rank_maker(ip,port,db,collection):
 
 		cited_rank = rankdata(cited_count_for_rank)
 		
-		#순위와 백분위를 구한다. 
+		#순위를 구한다. 
 		for item_id in id_iter_map:
 			index = id_iter_map[item_id]
 			collection_rank[item_id][year] = cited_rank[index]
-			collection_rank_percentile[item_id][year] = percentileofscore(cited_count_for_percentile, index)
+
+	for item_id in id_iter_map:
+		index = id_iter_map[item_id]
+		collection_client.update({"_id":item_id},{"$set":{"rank":collection_rank[item_id],"last_modified":time.time()}})
+
+
+def author_h_index_maker(ip,port,db,collection):
+	collection_client = MongoClient(ip, port)[db][collection]
+	item_client = MongoClient(ip, port)[db]["paper"]
+
+	author_h_index = {}
+	cited_counts_years = [[]]*66
+	past_time = time.time()
+	iterator = 0
+
+	for collection_id in collection_client.find():
+		if (iterator%10000 == 0):
+			print collection + " cited_count downloading : " + str(iterator) + "/" + str(collection_client.count()) +" takes: " + str((time.time() - past_time)) + " seconds"
+			past_time = time.time()
+
+		published = collection_id["published"]
+
+		for year in published:
+			item_list= published[year]
+			
+			for item_id in item_list:
+				#최종인용수를 만들고 그 사이의 랭킹을 구한다. 
+				item_cited_count = item_client.find_one({"_id":str(item_id)})["cited_count_sum"]
+				for i in range(0,66):
+					cited_counts_years[i].append(item_cited_count[i])
+
+				print cited_counts_years	
+				input()
+
+
+def test(ip,port,db):
+	#시간넣기 
+	collection_client = MongoClient(ip, port)[db]["test"]
+	#collection_client.save({"_id":"1", "time":time.time(),"temp":"2312312"})
+
+
+	collection_rank = {}
+
+	collection_cited_count = [[8],[9],[1],[1],[1],[7],[7],[8],[9]]
+	#일반 rank와 percentile rank를 같이 구한다. 
+	for year in range(0,1):
+
+		past_time = time.time()		
+
+		cited_count_for_rank = [-item[year] for item in collection_cited_count]
+		cited_count_for_percentile = [item[year] for item in collection_cited_count]
+		print cited_count_for_rank
+		print cited_count_for_percentile
+
+		cited_rank = rankdata(cited_count_for_rank)
+		#순위와 백분위를 구한다. 
+
+		print cited_rank
+
+
+
 
 	for item_id in id_iter_map:
 		index = id_iter_map[item_id]
 		collection_client.update({"_id":item_id},{"$set":{"rank":collection_rank[item_id],"rank_percent":collection_rank_percentile[item_id],"last_modified":time.time()}})
 
 
-	"""
-	collection_cited = {}
 
-	if (iterator%10000 == 0 ):
-		print collection + " cited_count uploading : " + str(iterator) + "/" + str(collection_client.count()) +" takes: " + str((time.time() - past_time)) + " seconds"
-		past_time = time.time()
-	iterator += 1
-
-	#저자가 발행한 논문들의 연도별 인용수 리스트가 필요하다 
-	for year in published:
-		item_list= published[year]
-	
-		for item_id in item_list:
-			cited_count = item_client.find_one({"_id":str(item_id)})["cited_count"]
-			#print str(item_id)+"'s cited_count: " + str(cited_count)
-			#p=""
-			for year in range(1950,2016):
-				if (str(year) not in collection_cited):
-					collection_cited[str(year)] = 0
-				collection_cited[str(year)] += int(cited_count[str(year)])
-	"""
 
 #최근 업데이트된 정보를 볼 수 있도록 시간을 추가하였다. 
 #이거 다 되면 author한번 더 돌릴 수 있도록 한다. 
@@ -147,21 +184,14 @@ port= 27017
 db = "DBLP_Citation_network_V8"
 #각 collection들의 연도별 cited count를 만들어 저장 -> ranking용 
 #collection_cited_count_maker(ip,port,db,"author")
-collection_cited_count_maker(ip,port,db,"venue")
+#collection_cited_count_maker(ip,port,db,"venue")
 
 #collection_rank_maker(ip,port,db,"author")
 #collection_rank_maker(ip,port,db,"venue")
 
 
+author_h_index_maker(ip,port,db,"author")
 #test(ip,port,db)
 #3. venue rank 구하기
 
 
-
-def test(ip,port,db):
-	#시간넣기 
-	collection_client = MongoClient('lamda.ml', 27017)[db]["test"]
-	#collection_client.save({"_id":"1", "time":time.time(),"temp":"2312312"})
-
-
-	collection_rank = {}
